feeefde7ea8f24ea53dbd0b10f8fb02342095044
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-24 02:07:26 +0545

    feat: added and removed some checks

--- src/middlewares/numberMiddlewares.ts
+++ src/middlewares/numberMiddlewares.ts
@@ -1,4 +1,4 @@
-import type { NextFunction, Request, Response } from 'express'
+import type { NextFunction, Request, Response } from 'express';
 
 /**
  * Checks if the number is in the correct format, and formats it to be used by the whatsapp-web.js library
@@ -12,26 +12,20 @@ export function numberFormater (req: Request, res: Response, next: NextFunction)
   if (number === '') {
     res.status(400).send({
       error: {
-        message: 'Missing number on request body'
+        message: 'missing number on request body'
       }
     })
     return
   }
-  if (!(/^\+[0-9]*?$/.test(number))) {
-    res.status(400).send({
-      error: {
-        message: 'Invalid number format. Use E.164 format, i.e. [+][country-code][10-digit-number-without-dashes]'
-      }
-    })
-    return
+  
+  // check if it's a group
+  if (number.endsWith("@g.us")) { } 
+  else {
+    number = number.replace(/\+/g, '')
+    if (!number.endsWith('@c.us')) {
+      number += '@c.us'
+    }
   }
-  // remove the + from the number
-  number = number.replace(/\+/g, '')
-
-  // add @c.us if it's not present because whatsapp ID must be in this format to be checked by the whatsapp-web.js library
-  if (!number.endsWith('@c.us')) {
-    number += '@c.us'
-  }
-  req.body.number = number
+  req.body.number = number;
   next()
 }
