d9b6d2ef9f60a7d377039f2ba4db8c08a3dc54f7
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-24 02:07:50 +0545

    feat: tidy up code, only create group route remains

--- src/routes/groupsRouter.ts
+++ src/routes/groupsRouter.ts
@@ -1,17 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
-import * as os from 'os';
 
-import { createAGroup, sendMessageToGroup } from '../controllers/groupsControllers';
+import { createAGroup } from '../controllers/groupsControllers';
 import { isClientReady } from "../middlewares/initMiddlewares";
-import { messageVerify } from "../middlewares/messageMiddlewares";
 
 const router = Router()
-
-const upload = multer({
-  storage: multer.diskStorage({ destination: os.tmpdir() }),
-  limits: { fileSize: 2048 * 1024 * 1024 }
-})
 
 router.post(
   '/',
@@ -19,12 +11,5 @@ router.post(
   createAGroup
 )
 
-router.post(
-  '/sendMessage',
-  isClientReady,
-  upload.any(),
-  messageVerify,
-  sendMessageToGroup
-)
 
 export default router
