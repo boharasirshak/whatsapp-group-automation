7a679881f34547882edd02405e95f6138359c1ff
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-24 02:06:10 +0545

    refactor: logs

--- src/middlewares/errorMiddlewars.ts
+++ src/middlewares/errorMiddlewars.ts
@@ -1,7 +1,7 @@
 import { type NextFunction, type Request, type Response } from 'express'
 
 export const basicErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => {
-  console.error(`[ERROR]: ${err.name} ${err.message}`)
+  console.error(`[error]: ${err.name} ${err.message}`)
   res.status(500).send({
     error: {
       name: err.name,
