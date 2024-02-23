4338450bcc770eb27bce21d0a655c164ce536a52
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-24 02:08:30 +0545

    feat: remove unecessary route

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,13 +1,8 @@
 import { Router } from "express";
 import multer from "multer";
 import * as os from "os";
-import {
-  getMe,
-  sendMessageToUser,
-} from "../controllers/usersControllers";
+import { getMe } from "../controllers/usersControllers";
 import { isClientReady } from "../middlewares/initMiddlewares";
-import { messageVerify } from "../middlewares/messageMiddlewares";
-import { numberFormater } from "../middlewares/numberMiddlewares";
 
 const upload = multer({
   storage: multer.diskStorage({ destination: os.tmpdir() }),
@@ -17,13 +12,5 @@ const upload = multer({
 const router = Router();
 
 router.get("/me", isClientReady, getMe);
-router.post(
-  "/sendMessage",
-  isClientReady,
-  upload.any(),
-  messageVerify,
-  numberFormater,
-  sendMessageToUser
-);
 
 export default router;
