36db71bbaa877a05c7d332da6bf52a1fc348a732
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-24 15:46:48 +0545

    feat: removed unecessary code

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,13 +1,6 @@
 import { Router } from "express";
-import multer from "multer";
-import * as os from "os";
 import { getMe } from "../controllers/usersControllers";
 import { isClientReady } from "../middlewares/initMiddlewares";
-
-const upload = multer({
-  storage: multer.diskStorage({ destination: os.tmpdir() }),
-  limits: { fileSize: 2048 * 1024 * 1024 },
-});
 
 const router = Router();
 
