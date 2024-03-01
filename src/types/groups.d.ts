5d8a7f7ca52688dfea0aa02d9361f24a6cb59162
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-01 10:57:05 +0545

    feat: added types

--- src/types/groups.d.ts
+++ src/types/groups.d.ts
@@ -1,4 +1,20 @@
+export type CreatedCustomer = {
+  name: string;
+  phone: string;
+  id?: string;
+  joined?: boolean = false;
+}
+
 export type CreatedGroup = {
-  title: string;
   id: string;
+  title: string;
+  customerType?: string;
+  customers?: CreatedCustomer[];
+  brandName?: string;
+  link1?: string;
+  link2?: string;
+  date1?: string;
+  date2?: string;
+  time?: string;
+  props?: string;
 };
