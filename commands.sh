e510f418e488800b52bc3ac4758618a8b418b457
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-25 15:09:13 +0545

    feat: better commands

--- commands.sh
+++ commands.sh
@@ -1,7 +1,3 @@
-sudo apt-get update
-sudo apt-get upgrade -y
+apt-get update
 
-sudo apt-get update && apt-get install -y wget --no-install-recommends \
-    && apt-get install -y libxshmfence1 libxrandr2 libxfixes3 libxcursor1 libxdamage1 libxi6 \
-    libxtst6 libnss3 libcups2 libxss1 libxcomposite1 libasound2 libpangocairo-1.0-0 libatk1.0-0 \
-    libatk-bridge2.0-0 libgtk-3-0 && rm -rf /var/lib/apt/lists/*
+sudo apt install -y gconf-service libgbm-dev libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
